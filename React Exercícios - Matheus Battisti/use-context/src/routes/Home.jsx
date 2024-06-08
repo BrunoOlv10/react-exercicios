2c3e7ec95d6e3ef5db705bc2e772f07129bfb067
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2024-06-08 01:12:32 -0300

    finished ex

--- React Exercícios - Matheus Battisti/use-context/src/routes/Home.jsx
+++ React Exercícios - Matheus Battisti/use-context/src/routes/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { useContext } from "react"
+
+import { ThemeContext } from "../context/ThemeContext"
 
 const Home = () => {
+
+    const {theme, toggleTheme} = useContext(ThemeContext)
+
   return (
     <div>
-      Home
+      <h1>Página inicial</h1>
+      <button onClick={toggleTheme}>Mudar tema</button>
+      <p>O tema atual é: {theme}</p>
     </div>
   )
 }
